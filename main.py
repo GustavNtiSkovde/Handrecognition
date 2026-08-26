bab50546a49772a5b29b7fa071d5625cb43dabe5
Drekk <201110792+GustavNtiSkovde@users.noreply.github.com>
2026-08-26 09:28:26 +0200

    Every letter img and when a hand is detected a test img is displayed over main window

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ mp_draw = mp.solutions.drawing_utils #draws landmarks and connections "on" the w
 
 cap = cv2.VideoCapture(0) #OpenCV uses camera index 0 often the base webcam in this case laptop webcam
 
+hand_sign_window = "Overlay Window"
+window_open = False
+popup_img = cv2.imread('img/Aa.jpeg')
+cv2.namedWindow(hand_sign_window, cv2.WINDOW_AUTOSIZE)
+
 while True:
     success, img = cap.read() #Reads the single frame from the video device 
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #OpenCV reads webcam using BGR(Blue Green Red) but Mediapipe wants RGB and COLOR_BGR2RGB converts it
@@ -17,12 +22,19 @@ while True:
         for handLms in result.multi_hand_landmarks: #if a hand is detected this for loop runs drawing landmarks
             mp_draw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS) #Draws landmarks and connections between handmarks
         print("Hand Detected") #Show hand detected in console
+        
+        cv2.imshow(hand_sign_window, popup_img)
+        window_open = True
     else:
+        if window_open == True: #If there is no detection of a hand but window_open is stll true, remove the hand_sign_window
+            cv2.destroyWindow(hand_sign_window) #Destroys hand_sign_window
+            window_open = False #Change varaible to false
         print("No Hand")
 
     cv2.imshow("Hand Tracker", img) #Opens window called handtracker which shows the landmarks and connections
     if cv2.waitKey(1) & 0xFF == ord('q'): #If q is pressed, close window and the loop breaks
         break
+    
 
 cap.release() #Turns of webcam
 cv2.destroyAllWindows() #Closes handtracker window
